6aadd085f9f237be20a858081eb6753d81347c98
RalfWerner <45426380+RalfWerner@users.noreply.github.com>
2019-06-06 15:42:58 +0200

    Update touch.c

--- touch.c
+++ touch.c
@@ -1,3 +1,7 @@
+#include <X11/Xutil.h>
+void WinNew(){;};void ColInfo(){;}; int x,y,x4,y4,xd,yd,i9=0,nw=
+ PointerMotionMask | ButtonPressMask | ButtonReleaseMask | ButtonMotionMask ;
+#include <X11/cursorfont.h>
 #include <X11/extensions/XInput2.h>
 Display* dpy; Window wd,wx; XEvent ev; Screen *screen;int i,j,k,l,m=0,n;
 
@@ -33,4 +37,26 @@ int main(int n0,char** A) /* Open Display (connection to X-Server) */
  } XIFreeDeviceInfo(info);
  if (l) k=XISelectEvents(dpy,wd,masks,l);
   else printf("E=> XITouchClass missing\n");
+
+/* exclusive Mouse pointer with Cursor 34 PointerMotionHintMask| return 1; */
+ WinNew(A[1],600,600); j=XC_crosshair; float v;
+ printf("\nLoop until point selected with MB3 k/l:%d/%d cursor=%d\n",k,l,j);
+ do {if (j>0) {Cursor cursor=XCreateFontCursor(dpy,j); j=-j;
+       k=XGrabPointer(dpy,wd,False,nw,GrabModeAsync,
+         GrabModeAsync,wd,cursor,CurrentTime);
+       if (k != GrabSuccess) {printf("can't grab the mouse %d\n",k); return 1;}}
+     XNextEvent(dpy,&ev); xd=x-ev.xbutton.x; yd=y-ev.xbutton.y;
+     x=ev.xbutton.x; y=ev.xbutton.y; v=sqrt(xd*xd+yd*yd);
+     if (ev.type==6 && v>90.) printf(" v(%d,%d)=%d ", xd,yd,(int)v);
+     if (ev.type==GenericEvent) {XGenericEventCookie* genev=&ev.xcookie;
+       XGetEventData(dpy,genev); printf(" Gen->%d ext:%d data:%d",genev->evtype,
+        ev.xcookie.extension,genev->data); XFreeEventData(dpy,genev);}
+     if (i-ev.type) printf(" t->%d,x=%d,y=%d, MB%d/%d\n",ev.type,
+     x,y,ev.xbutton.button,ev.xbutton.state);
+     if ((i=ev.type)==ButtonPress) {x4=ev.xbutton.x_root; y4=ev.xbutton.y_root;}
+     else if (ev.type == ButtonRelease){j=3-j-2*ev.xbutton.button;
+      printf("Diff:%d,% x:d, y:%d, j:%d ",i9++,x-x4,y-y4,j);
+      ColInfo(dpy,ev.xbutton.x_root,ev.xbutton.y_root); }
+    } while (ev.type != ButtonRelease || ev.xbutton.button!=3);
+ XUngrabPointer(dpy,CurrentTime); if (i9>7) return 9;
 }
